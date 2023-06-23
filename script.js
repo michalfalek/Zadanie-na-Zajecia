e6d8f28743fe2c3115fd4a2fbea60a9ea49afe76
michalfalek <137315605+michalfalek@users.noreply.github.com>
2023-06-23 15:51:15 +0200

    Dodanie styles.css
    Dodanie styli i usunięcie komentarza w js

---
+++ script.js
@@ -0,0 +1,41 @@
+// script.js
+let currentExpression = ''; // Aktualne wyrażenie matematyczne
+
+// Funkcja do aktualizacji pola tekstowego
+function updateResult() {
+  const resultField = document.getElementById('result');
+  resultField.value = currentExpression;
+}
+
+// Funkcja do dodawania wartości do aktualnego wyrażenia
+function appendValue(value) {
+  currentExpression += value;
+  updateResult();
+}
+
+// Funkcja do dodawania operatora do aktualnego wyrażenia
+function appendOperator(operator) {
+  currentExpression += operator;
+  updateResult();
+}
+
+// Funkcja do czyszczenia aktualnego wyrażenia
+function clearResult() {
+  currentExpression = '';
+  updateResult();
+}
+
+// Funkcja do obliczania wyniku
+function calculate() {
+  const resultField = document.getElementById('result');
+  let result;
+
+  try {
+    result = eval(currentExpression); // Wykorzystanie funkcji eval() do obliczenia wyrażenia
+  } catch (error) {
+    result = 'Błąd';
+  }
+
+  resultField.value = result;
+  currentExpression = ''; // Zresetowanie wyrażenia po obliczeniach
+}
